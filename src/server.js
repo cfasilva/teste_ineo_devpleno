98798270493ba14dbe59429bef0846648436b764
Cleber Abreu <cleberfasilva@gmail.com>
2024-05-16 21:48:53 -0300

    add routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-import express, { json, urlencoded } from 'express'
+import express from 'express'
+import routes from './routes.js'
 
 const app = express()
 
-app.use(json())
-app.use(urlencoded({ extended: false }))
+app.use(express.json())
+app.use(routes)
 
-app.listen(3333, () => console.log(`Running at http://localhost:3333`))
+app.listen(3333, () => console.log(`Running...`))
