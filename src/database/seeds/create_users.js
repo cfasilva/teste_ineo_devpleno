8d435cec80a226a44d5b5f56862b9dcd801c9f5a
Cleber Abreu <cleberfasilva@gmail.com>
2024-05-17 15:11:50 -0300

    remove user seed

--- src/database/seeds/create_users.js
+++
@@ -1,6 +0,0 @@
-export async function seed(knex) {
-  await knex('users').insert([
-    { name: 'Ruan Nunes', email: 'ruan.nunes@ineodigital.com.br', password: '******' },
-    { name: 'Cleber Silva', email: 'cleberfasilva@gmail.com', password: '******' },
-  ])
-}
